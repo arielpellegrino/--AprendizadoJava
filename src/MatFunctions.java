1136cb9aadc5f88f7dfb5d3e63d48c88454c3933
Ariel Pellegrino <ariel.pell@gmail.com>
2021-09-14 21:19:03 -0300

    finalizando calculos matematicos

--- src/MatFunctions.java
+++ src/MatFunctions.java
@@ -4,8 +4,13 @@ public class MatFunctions {
     /*  A= Math.pow(y, x); Variavel A recebe o resultado de x elevado a y
     /* A = Math.abs(x); Variavel A recebe o valor absoluto de x
 
+    sqrt = raiz quadrada
+    pow = potenciação
+    abs = valor absoluto
+
      */
 
+/*
     public static void main(String[] args){
 
         double x = 3.0;
@@ -34,6 +39,12 @@ public class MatFunctions {
 
         System.out.println("Valor absoluto de " + y + " = " + A);
         System.out.println("Valor absoluto de " + z + " = " + B);
+
+*/
+        derrlta = Math.pow(b, 2.0) - 4*a*c;
+
+        x1 = (-b + Math.sqrt(delta)) / (2.0 * a);
+        x2 = (-b - Math.sqrt(delta)) / (2.0 * a);
     }
 
 }
