ff9a55bfd128f4fa80406dcff4a1c3dd5b219b9a
Ariel Pellegrino Iborra <ariel.iborra@sensedia.com>
2022-04-09 17:30:48 -0300

    up

--- src/triangle.java
+++ src/triangle.java
@@ -1,5 +1,13 @@
+
 public class triangle {
     public double a;
     public double b;
     public double c;
+
+    public double area(){
+        double p = (a + b + c) / 2.0;
+        return Math.sqrt(p * p (p - a)) * (p - b) * (p - c));
+
+    }
+
 }
